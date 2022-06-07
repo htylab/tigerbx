6dabe919d51e8ef0fbe78cd33a0830db091aa5b4
htylab <10368378+htylab@users.noreply.github.com>
2022-06-07 12:29:24 +0800

    reduce modelname to model

--- src/tigerseg/console/__main__.py
+++ src/tigerseg/console/__main__.py
@@ -17,13 +17,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', metavar='input_file',type=str, help='Path to the input image, can be a folder for the specific format(nii.gz)')
     parser.add_argument('output_dir', metavar='output_dir', type=path, help='Filepath for output segmentation')
-    parser.add_argument('--modelname', default='', type=str, help='specifies the modelname')
+    parser.add_argument('--model', default='', type=str, help='specifies the modelname')
     parser.add_argument('--GPU',default='False',type = strtobool, help='True: GPU, False: CPU, default: False, CPU')
     parser.add_argument('--report',default='True',type = strtobool, help='Produce additional reports')
     args = parser.parse_args()
 
     input_file_list = glob.glob(args.input_file)
-    segment.apply_files(model_name=args.modelname,
+    segment.apply_files(model_name=args.model,
                         input_file_list=input_file_list,
                         output_dir=args.output_dir,                        
                         GPU=args.GPU,
