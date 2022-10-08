175b8aaa0b38c9365ba1c2a8dc48465752aaacdb
htylab <10368378+htylab@users.noreply.github.com>
2022-10-08 10:56:52 +0800

    update

--- src/tigerseg/console/__aseg2__.py
+++ src/tigerseg/console/__aseg2__.py
@@ -14,8 +14,8 @@ def path(string):
 
 def main():
 
-    default_model = 'mprage_v0001_aseg43_MXRW*mprage_v0002_bet_kuo' #weng's thesis
-    #default_model = 'mprage_v0003_aseg43_WangM1*mprage_v0002_bet_kuo' #wang's thesis
+    #default_model = 'mprage_v0001_aseg43_MXRW*mprage_v0002_bet_kuo' #weng's thesis
+    default_model = 'mprage_v0003_aseg43_WangM1*mprage_v0002_bet_kuo' #wang's thesis
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', metavar='INPUT_FILE', required=True, type=str, nargs='+', help='Path to the input image, can be a folder for the specific format(nii.gz)')
