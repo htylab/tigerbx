a01539da79fcb082422d8cbe273cc05dea9f03f6
kuogeo123 <kuogeo123@gmail.com>
2022-11-06 09:28:09 +0800

    update compile exe

--- src/exe/tigerbet/compile.bat
+++ src/exe/tigerbet/compile.bat
@@ -1,1 +1,3 @@
-pyinstaller -c --icon=ico.ico -F --add-data onnxruntime_providers_shared.dll;onnxruntime\capi --add-data mprage_v0004_bet_full.onnx;tigerseg\models tigerbet.py
+pipenv install --site-packages https://github.com/htylab/tigerseg/archive/main.zip
+pipenv install pyinstaller
+pipenv run pyinstaller -c --icon=ico.ico -F --add-data onnxruntime_providers_shared.dll;onnxruntime\capi --add-data mprage_v0004_bet_full.onnx;tigerseg\models tigerbet.py
