de3c8dd69bdf97b3c60cd12eb8abb10831ec706c
htylab <10368378+htylab@users.noreply.github.com>
2022-11-15 06:46:57 +0800

    update

--- tigerseg/exe/tigervdm/compile.bat
+++
@@ -1,3 +0,0 @@
-pipenv install --site-packages https://github.com/htylab/tigerseg/archive/main.zip
-pipenv install pyinstaller
-pipenv run pyinstaller -c --icon=ico.ico -F --add-data onnxruntime_providers_shared.dll;onnxruntime\capi --add-data mprage_v0004_bet_full.onnx;tigerseg\models --add-data mprage_v0002_bet_kuor128.onnx;tigerseg\models --add-data mprage_v0005_aseg43_full.onnx;tigerseg\models --add-data mprage_v0001_aseg43_MXRWr128.onnx;tigerseg\models  tigerbet.py
