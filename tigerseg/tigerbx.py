3779cefb784992497706aac8bf9be88aa02d646b
htylab <10368378+htylab@users.noreply.github.com>
2022-12-01 10:06:35 +0800

    update

--- tigerseg/tigerbx.py
+++ tigerseg/tigerbx.py
@@ -103,17 +103,27 @@ def run_args(args):
     #model_name = args.model
     #model_aseg = 'mprage_v0006_aseg43_full.onnx'
 
+    default_model = dict()
+    default_model['bet128'] = 'mprage_bet_v001_kuor128.onnx'
+    default_model['aseg128'] = 'mprage_aseg43_v001_MXRWr128.onnx'
+
+    default_model['bet'] = 'mprage_bet_v002_full.onnx'
+    default_model['aseg'] = 'mprage_aseg43_v005_crop.onnx'
+    default_model['dkt'] = 'mprage_dkt_v001_f16r256.onnx'
+    default_model['dgm'] = 'mprage_aseg43_v005_crop.onnx'
+
+
     if args.fast:
-        model_bet = 'mprage_bet_v001_kuor128.onnx'
-        model_aseg = 'mprage_aseg43_v001_MXRWr128.onnx'
+        model_bet = default_model['bet128']
+        model_aseg = default_model['aseg128']
         
     else:
-        model_bet = 'mprage_bet_v002_full.onnx'
+        model_bet = default_model['bet']
         #model_aseg = 'mprage_v0006_aseg43_full.onnx'
         #model_aseg = 'mprage_aseg43_v002_WangM1r256.onnx'
-        model_aseg = 'mprage_aseg43_v005_crop.onnx'
-    model_dkt = 'mprage_dkt_v001_f16r256.onnx'
-    model_dgm = 'mprage_dgm12_v001_wangM1V2.onnx'
+        model_aseg = default_model['aseg']
+    model_dkt = default_model['dkt']
+    model_dgm = default_model['dgm']
 
 
 
