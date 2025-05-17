c6a6352fb6a4bdf843f79c11894a9e9cd3e02787
htylab <10368378+htylab@users.noreply.github.com>
2025-05-17 11:36:02 +0800

    Update __init__.py

--- tigerbx/__init__.py
+++ tigerbx/__init__.py
@@ -3,4 +3,4 @@ from tigerbx.validate import val
 from tigerbx.apply_displacement import transform
 from tigerbx.gdmi import gdm
 from tigerbx.hlc70 import hlc
-__version__ = '0.1.19'
+__version__ = '0.1.20'
