5c96cfc61c595ab119e4ba9fa66316f2b7872047
htylab <tyhuang@mail.ntust.edu.tw>
2026-02-22 20:10:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-classifiers = [
-    'Intended Audience :: Developers',
-    'Programming Language :: Python :: 3',
-    'License :: Free for non-commercial use',
-    "Operating System :: OS Independent"
-]
-
 setup(
      name='tigerbx',
-
-     version='0.2.1',
-     description='Processing MRI images based on deep-learning',
-     long_description=long_description,
-     long_description_content_type='text/markdown',
-     url='https://github.com/htylab/tigerbx',
-
-     author='AI & Scientific Computing Lab, Taiwan Tech',
-     author_email='',
+     version='0.2.2',
      license='CC BY-NC 4.0',
      license_files=['LICENSE'],
-     classifiers=classifiers,
 
-     keywords='MRI brain segmentation',
      packages=find_packages(),
      package_data={
         'tigerbx': ['template/*.nii.gz'],  # include the MNI152 template
